be4a8ec721bd27f164b948426242f46eb9e4f835
xzaxza <xzaxza12@gmail.com>
2023-12-19 21:16:50 +0000

    added templates, updated urls

--- mblog/mblog/settings.py
+++ mblog/mblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1q%v$bg#2be8+)u%36*8@+u@n%43t0dr8o1$hqn%4es3mo&_5l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mysite',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mblog/mysite/views.py
+++ mblog/mysite/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.shortcuts import render, redirect
+
+from mysite.models import Post
+
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists =list()
+    now = datetime.now()
+    return render(request,"index.html",locals())
+
+def showpost(request,slug):
+    post = Post.objects.get(slug=slug)
+    try:
+        if post != None:
+            return render(request, "post.html", locals())
+    except:
+        return redirect("/")
+
+
+
